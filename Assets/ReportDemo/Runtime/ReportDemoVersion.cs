8ad1e28e302a77452f53044ef48930c7b79877bd
majyeuem2542004 <tranvietbg254@gmail.com>
2026-09-03 01:56:53 +0700

    Revert "Track ReportDemo runtime scripts"
    This reverts commit 16b18436d5e3aac08852a959059f136ab8093ae4.

--- Assets/ReportDemo/Runtime/ReportDemoVersion.cs
+++
@@ -1,8 +0,0 @@
-namespace Android2D.ReportDemo
-{
-    /// <summary>Version marker for the report demo handoff.</summary>
-    public static class ReportDemoVersion
-    {
-        public const string Value = "1.0.0";
-    }
-}
